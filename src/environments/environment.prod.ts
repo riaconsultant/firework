ecdfe6f2cd9536a87e370d1ff20ab9ff8b8f3d84
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2018-01-18 23:23:07 +0530

    environment setting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "AIzaSyD2lqpm0IIqSOqSc6xlgYhTnnseyKRphuQ",
+  authDomain: "society-65706.firebaseapp.com",
+  databaseURL: "https://society-65706.firebaseio.com",
+  projectId: "society-65706",
+  storageBucket: "society-65706.appspot.com",
+  messagingSenderId: "814110681577"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,11 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyD2lqpm0IIqSOqSc6xlgYhTnnseyKRphuQ",
+  authDomain: "society-65706.firebaseapp.com",
+  databaseURL: "https://society-65706.firebaseio.com",
+  projectId: "society-65706",
+  storageBucket: "society-65706.appspot.com",
+  messagingSenderId: "814110681577"
 };
