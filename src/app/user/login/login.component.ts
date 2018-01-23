d107e07d62883a3d96c15cb6f20f07d13b236237
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2018-01-23 23:17:59 +0530

    ngModelOptions added

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -5,7 +5,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit,OnDestroy {
-
+  model={
+    name:"Manoj"
+  }
   constructor() { }
 
   ngOnInit() {
