bb36ad59f3f9d8f55bcea8891aa0ea71d14b4e6e
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2018-01-25 00:01:26 +0530

    app module and route changes

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -10,9 +10,9 @@ import { AuthGuard } from './auth.guard';
 import { RouterModule,Routes } from '@angular/router';
 
 const appRoute:Routes=[
-  { path:"login",loadChildren:'./user/user.module#UserModule'},
+  //{ path:"login",loadChildren:'./user/user.module#UserModule'},
   // { path:'',component:layout.PublicComponent,children:[
-  //   {path:"login", component:user.LoginComponent},
+     {path:"login", component:user.LoginComponent},
   //   {path:"register",component: user.RegistrationComponent},
   //   //{path:"resetpassword",component: ResetpasswordComponent},
   //   {path:"pagenotfound",component:user.PagenotfoundComponent},
